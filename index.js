7cef6741f4eab96d48cf464378d89598cd3ac1c7
Roberto Ibarra <r.ibarrabernal@outlook.com>
2022-04-14 11:00:55 -0700

    basic functionality

--- index.js
+++ index.js
@@ -1,9 +1,47 @@
-const express = require("express");
+const morgan = require("morgan");
+const path = require("path");
+const socketIO = require('socket.io');
+const http = require('http')
 
+const express = require("express");
 const app = express();
 
+let server = http.createServer(app)
+let io = socketIO(server)
+
+// Middlewares
+app.use(morgan("dev"))
+app.use(express.json());
+app.use('/statics', express.static(path.join(__dirname, 'statics')))
 
 
-app.listen(3000,()=>{
+// Routes
+app.get("/",(req,res)=>{
+    res.sendFile(path.join(__dirname,"statics/index.html"))
+});
+
+
+io.on('connection', function(socket) {
+    console.log('A user connected');
+ 
+    //Whenever someone disconnects this piece of code executed
+    socket.on('disconnect', function () {
+       console.log('A user disconnected');
+    });
+
+    socket.on("order",(message)=>{
+        console.log(message);
+    })
+ });
+
+
+
+
+
+
+// Routes
+
+
+server.listen(3000,()=>{
     console.log("all good");
 });
