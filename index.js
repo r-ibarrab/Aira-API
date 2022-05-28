32467960333a5fbad956a3e051bfd345b35ba988
Roberto Ibarra <r.ibarrabernal@outlook.com>
2022-05-27 20:09:15 -0700

    a

--- index.js
+++ index.js
@@ -14,9 +14,9 @@ let io = socketIO(server)
 app.use(morgan("dev"))
 app.use(express.json());
 
-// app.get("/",(req,res)=>{
-//     res.sendFile(path.join(__dirname,"index.html"));
-// })
+app.get("/",(req,res)=>{
+    res.sendFile(path.join(__dirname,"index.html"));
+})
 
 io.on('connection', function(socket) {
     console.log('A user connected');
